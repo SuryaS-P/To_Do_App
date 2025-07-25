f3537449c46e3634cc1ecb6cc8e2546955ddc136
Surya Singh-Prasad <surya.singhprasad@gmail.com>
2025-07-25 13:04:29 -0400

    Initial Commit

---
+++ To_Do_App/To_Do_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_AppApp.swift
+//  To_Do_App
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
